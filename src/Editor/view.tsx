3d9e788fdfcacbb70f5439280d3409b6808708b4
Géraud <geraud.henrion@nartex.fr>
2019-07-25 14:16:38 +0200

    cleanup rendering

--- src/Editor/view.tsx
+++ src/Editor/view.tsx
@@ -3,7 +3,13 @@ import { State } from './index'
 import { Stream } from 'xstream'
 import { VNode } from '@cycle/dom'
 import dropRepeats from 'xstream/extra/dropRepeats'
-import { parse, BufferContent, PortalsDict } from '../lang'
+import {
+  parse,
+  BufferContent,
+  PortalsDict,
+  PortalInstance,
+  Destination,
+} from '../lang'
 
 export function view(state$: Stream<State>): Stream<VNode> {
   return state$
@@ -11,10 +17,12 @@ export function view(state$: Stream<State>): Stream<VNode> {
     .map(state => {
       return { buffer: state.buffer, ...parse(state.buffer) }
     })
-    .map(editor)
+    .map(EditorContent)
 }
 
-function editor({
+const OFFSET = 1
+
+function EditorContent({
   content,
   portals,
   buffer,
@@ -25,77 +33,95 @@ function editor({
   buffer: string
   left?: number
 }) {
-  const children = content.map((x, i) => {
-    const key = i
-    if (x.type === 'text') {
-      return text(buffer, x, key, parentLeft)
-    }
-    const matchingPortal = portals[x.for]
-    const left = Math.min(x.left, matchingPortal.left)
-    const width = Math.max(x.right, matchingPortal.right) - left + 1 + 10
-
-    if (x.type === 'opening' || x.type === 'ending') {
-      return text(buffer, x, key, left, width)
+  const children = content.map((line, index) => {
+    if (line.type === 'text') {
+      return TextNode({ ...line, left: parentLeft }, index)
     }
 
-    if (x.type === 'destination') {
-      if (!matchingPortal) {
-        return text(buffer, x, key, left, width)
+    const matchingPortal = portals[line.for]
+    const left = Math.max(0, Math.min(line.left, matchingPortal.left) - OFFSET)
+    const width =
+      Math.max(line.right, matchingPortal.right) - left + 1 + 2 * OFFSET
+
+    if (line.type === 'opening' || line.type === 'ending') {
+      return TextNode({ ...line, left, width }, index)
+    }
+
+    if (line.type === 'destination') {
+      if (matchingPortal) {
+        return RenderPortalInstance(
+          line,
+          { ...matchingPortal, left, width },
+          index,
+        )
       }
-      return (
-        <div
-          className="portal-instance"
-          style={{
-            'margin-left': `calc(var(--ch) * ${left})`,
-            'max-width': `calc(var(--ch) * ${width})`,
-            overflow: 'hidden',
-          }}
-          scrollLeft={left * 12}
-          hook={{
-            update: vnode => {
-              if (vnode.elm) {
-                let elm = vnode.elm as HTMLElement
-                elm.scrollLeft = 12 * left
-
-                elm.onscroll = () => {
-                  elm.scrollLeft = 12 * left
-                }
-              }
-            },
-          }}
-        >
-          {text(buffer, x, key, left, width)}
-          {editor({
-            content: matchingPortal.content,
-            portals,
-            buffer,
-            left: matchingPortal.left,
-          })}
-        </div>
-      )
+      return TextNode({ ...line, left, width }, index)
     }
     return null
   })
 
   return <div className="editor">{children}</div>
-}
 
-function text(
-  buffer: string,
-  x: { start: number, end: number, type?: string },
-  key?: string | number,
-  left?: number,
-  width?: number,
-) {
-  return (
-    <Buffer
-      value={buffer}
-      key={key}
-      className={x.type || ''}
-      start={x.start}
-      end={x.end}
-      width={width}
-      left={left}
-    />
-  )
+  function TextNode(
+    x: {
+      start: number
+      end: number
+      type?: string
+      left?: number
+      width?: number
+    },
+    index: number,
+  ) {
+    return (
+      <Buffer
+        key={`texnode-${index}`}
+        className={`-${x.type}`}
+        value={buffer}
+        start={x.start}
+        end={x.end}
+        width={x.width}
+        left={x.left}
+      />
+    )
+  }
+
+  function RenderPortalInstance(
+    line: Destination,
+    portal: PortalInstance,
+    index: number,
+  ) {
+    function hook(vnode: VNode) {
+      if (vnode.elm) {
+        let elm = vnode.elm as HTMLElement
+        requestAnimationFrame(() => {
+          elm.scrollLeft = 12 * portal.left
+        })
+        elm.onscroll = () => {
+          elm.scrollLeft = 12 * portal.left
+        }
+      }
+    }
+
+    return (
+      <div
+        className="portal-instance"
+        key={`portalInstance-${index}`}
+        style={{
+          'margin-left': `calc(var(--ch) * ${portal.left})`,
+          'max-width': `calc(var(--ch) * ${portal.width})`,
+          overflow: 'hidden',
+        }}
+        scrollLeft={portal.left * 12}
+        hook={{ insert: hook, update: hook }}
+      >
+        {TextNode({ ...line, left: portal.left, width: portal.width }, index)}
+        {EditorContent({
+          content: portal.content,
+          portals,
+          buffer,
+          left: portal.left,
+        })}
+      </div>
+    )
+  }
 }
--- src/lang/types.ts
+++ src/lang/types.ts
@@ -1,13 +1,13 @@
 export interface Dict<T> {
-    [id: string]: T
+  [id: string]: T
 }
 
 export type BufferContent = Placeholder | Destination | Text | Opening | Ending
 export type PortalsDict = Dict<Portal>
 
 export interface Context {
-    content: Array<BufferContent>
-    portals: Dict<Portal>
+  content: Array<BufferContent>
+  portals: Dict<Portal>
 }
 
 type LineCount = number
@@ -15,49 +15,46 @@ type CharCount = number
 export type Id = string
 
 export interface Portal {
-    id?: Id
-    start: LineCount
-    end: LineCount
-    left: CharCount
-    right: CharCount
-    content: Array<BufferContent>
-    warped?: boolean
+  id?: Id
+  start: LineCount
+  end: LineCount
+  left: CharCount
+  right: CharCount
+  content: Array<BufferContent>
+  warped?: boolean
 }
 
 export interface PortalInstance extends Portal {
-    top: LineCount
-    left: CharCount
-    right: CharCount
-    height: LineCount
+  width: LineCount
 }
 
 export interface Opening extends Ref {
-    type: 'opening'
+  type: 'opening'
 }
 
 export interface Ending extends Ref {
-    type: 'ending'
+  type: 'ending'
 }
 
 export interface Placeholder extends Ref {
-    type: 'placeholder'
+  type: 'placeholder'
 }
 
 export interface Destination extends Ref {
-    type: 'destination'
+  type: 'destination'
 }
 
 export interface Text extends Base {
-    type: 'text'
+  type: 'text'
 }
 
 interface Ref extends Base {
-    for: Id
+  for: Id
 }
 
 export interface Base {
-    start: LineCount
-    end: LineCount
-    left: CharCount
-    right: CharCount
+  start: LineCount
+  end: LineCount
+  left: CharCount
+  right: CharCount
 }
