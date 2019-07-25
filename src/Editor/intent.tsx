8e29d45e430201356e08e1224f018d66649f9c7b
Funx <geraud.henrion@gmail.com>
2019-07-25 21:23:07 +0200

    fix type error

--- src/Editor/intent.tsx
+++ src/Editor/intent.tsx
@@ -35,9 +35,8 @@ export function intent({ DOM, selection, state }: Sources) {
             return {
                 start,
                 end: start + height,
-                height,
+                width,
                 right: width - left,
-                top: start,
                 left: left,
                 content: []
             }
