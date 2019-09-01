866b4e82f61fbb5e32759a23b6063f8f2e394178
Funx <geraud.henrion@gmail.com>
2019-09-01 23:33:55 +0200

    fix tests

--- src/lang/parse/index.test.ts
+++ src/lang/parse/index.test.ts
@@ -6,7 +6,7 @@ import { cleanupContent } from './cleanupContent'
 test('empty text', () => {
   expect(cleanupContent(parse(''))).toEqual({
     portals: {},
-    content: [{ type: 'text', start: 0, end: 0, left: 0, right: 0 }],
+    content: [[{ type: 'text', start: 0, end: 0, left: 0, right: 0 }]],
   })
 })
 
@@ -19,7 +19,7 @@ How are you ?
 `)),
   ).toEqual({
     portals: {},
-    content: [{ type: 'text', start: 0, end: 4, left: 0, right: 13 }],
+    content: [[{ type: 'text', start: 0, end: 4, left: 0, right: 13 }]],
   })
 })
 
@@ -33,7 +33,7 @@ Hello guys
 How are you ?
 `)),
   ).toEqual({
-    content: [{ type: 'text', start: 0, end: 6, left: 0, right: 13 }],
+    content: [[{ type: 'text', start: 0, end: 6, left: 0, right: 13 }]],
     portals: {},
   })
 })
@@ -52,12 +52,12 @@ How are you ?
 `)),
   ).toEqual({
     content: [
-      { type: 'text', start: 0, end: 0, left: 0, right: 0 },
-      { type: 'opening', start: 1, end: 1, left: 0, right: 12, for: '1' },
-      { type: 'ending', start: 5, end: 5, left: 0, right: 13, for: '1' },
-      { type: 'text', start: 6, end: 7, left: 0, right: 13 },
-      { type: 'destination', start: 8, end: 8, left: 0, right: 10, for: '1' },
-      { type: 'text', start: 9, end: 9, left: 0, right: 0 },
+      [{ type: 'text', start: 0, end: 0, left: 0, right: 0 }],
+      [{ type: 'opening', start: 1, end: 1, left: 0, right: 12, for: '1' }],
+      [{ type: 'ending', start: 5, end: 5, left: 0, right: 13, for: '1' }],
+      [{ type: 'text', start: 6, end: 7, left: 0, right: 13 }],
+      [{ type: 'destination', start: 8, end: 8, left: 0, right: 10, for: '1' }],
+      [{ type: 'text', start: 9, end: 9, left: 0, right: 0 }],
     ],
     portals: {
       '1': {
@@ -66,7 +66,7 @@ How are you ?
         end: 4,
         left: 0,
         right: 10,
-        content: [{ type: 'text', start: 2, end: 4, left: 0, right: 10 }],
+        content: [[{ type: 'text', start: 2, end: 4, left: 0, right: 10 }]],
         warped: true,
       },
     },
@@ -83,7 +83,7 @@ How are you ?
 // WARP #1
 `)),
   ).toEqual({
-    content: [{ type: 'text', start: 0, end: 6, left: 0, right: 13 }],
+    content: [[{ type: 'text', start: 0, end: 6, left: 0, right: 13 }]],
     portals: {},
   })
 })
@@ -105,13 +105,13 @@ How are you ?
 `)),
   ).toEqual({
     content: [
-      { type: 'text', start: 0, end: 0, left: 0, right: 0 },
-      { type: 'opening', start: 1, end: 1, left: 0, right: 12, for: '1' },
-      { type: 'ending', start: 6, end: 6, left: 0, right: 13, for: '1' },
-      { type: 'text', start: 7, end: 8, left: 0, right: 13 },
-      { type: 'destination', start: 9, end: 9, left: 0, right: 10, for: '1' },
-      { type: 'destination', start: 10, end: 10, left: 0, right: 10, for: '2' },
-      { type: 'text', start: 11, end: 11, left: 0, right: 0 },
+      [{ type: 'text', start: 0, end: 0, left: 0, right: 0 }],
+      [{ type: 'opening', start: 1, end: 1, left: 0, right: 12, for: '1' }],
+      [{ type: 'ending', start: 6, end: 6, left: 0, right: 13, for: '1' }],
+      [{ type: 'text', start: 7, end: 8, left: 0, right: 13 }],
+      [{ type: 'destination', start: 9, end: 9, left: 0, right: 10, for: '1' }],
+      [{ type: 'destination', start: 10, end: 10, left: 0, right: 10, for: '2' }],
+      [{ type: 'text', start: 11, end: 11, left: 0, right: 0 }],
     ],
     portals: {
       '1': {
@@ -119,9 +119,9 @@ How are you ?
         start: 2,
         end: 5,
         content: [
-          { type: 'text', start: 2, end: 2, left: 0, right: 5 },
-          { type: 'opening', start: 3, end: 3, left: 0, right: 12, for: '2' },
-          { type: 'ending', start: 5, end: 5, left: 0, right: 13, for: '2' },
+          [{ type: 'text', start: 2, end: 2, left: 0, right: 5 }],
+          [{ type: 'opening', start: 3, end: 3, left: 0, right: 12, for: '2' }],
+          [{ type: 'ending', start: 5, end: 5, left: 0, right: 13, for: '2' }],
         ],
         warped: true,
         left: 0,
@@ -132,7 +132,7 @@ How are you ?
         id: '2',
         start: 4,
         end: 4,
-        content: [{ type: 'text', start: 4, end: 4, left: 0, right: 4 }],
+        content: [[{ type: 'text', start: 4, end: 4, left: 0, right: 4 }]],
         warped: true,
         left: 0,
         right: 4,
