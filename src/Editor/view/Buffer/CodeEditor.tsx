2a55feafeb1d5b37883ec986fbfb2742f6814b1e
Funx <geraud.henrion@gmail.com>
2020-02-26 23:27:26 +0100

    add zones support for CodeEditor

---
+++ src/Editor/view/Buffer/CodeEditor.tsx
@@ -0,0 +1,176 @@
+import { flatten } from 'ramda'
+import { VNode } from '@cycle/dom'
+import { BoundingRect } from 'src/lang'
+import { editor as mEditor, Selection } from 'monaco-editor'
+import { makeSnabbdomElement, Dict } from 'cycle-element/src'
+
+type CodeEditorProps = {
+  selection?: Selection
+  value: string
+  start: number
+  left: number
+  children?: VNode[]
+  zones: Zone[]
+  cuts: BoundingRect[]
+} & Dict
+
+type Zone = {
+  afterLineNumber: number
+  afterColumn: number
+  heightInLines: number
+  id?: string
+}
+
+let model: mEditor.ITextModel
+export const CodeEditor = makeSnabbdomElement<CodeEditorProps>(
+  elm => {
+    model = model || mEditor.createModel('', 'javascript')
+
+    let prevProps: Partial<CodeEditorProps> = {}
+    let editor = mEditor.create(elm, {
+      language: 'javascript',
+      roundedSelection: true,
+      scrollBeyondLastLine: false,
+      readOnly: false,
+      theme: 'vs-dark',
+      lineNumbers: 'on',
+      scrollbar: {
+        vertical: 'hidden',
+      },
+      automaticLayout: false,
+      minimap: {
+        enabled: false,
+      },
+      model,
+    })
+
+    let onDidScrollChange = editor.onDidScrollChange(() => {
+      const scrollTop = (prevProps.start || 0) * 18
+      const scrollLeft = (prevProps.left || 0) * 7.22
+      if (editor.getScrollTop() !== scrollTop) {
+        editor.setScrollTop(scrollTop)
+      }
+      if (editor.getScrollLeft() !== scrollLeft) {
+        editor.setScrollLeft(scrollLeft)
+      }
+    })
+
+    let onDidChangeCursorSelection = editor.onDidChangeCursorSelection(e => {
+      emit(elm, 'monaco-selectionchange', e)
+    })
+
+    editor.onDidChangeModelContent(e => {
+      emit(elm, 'monaco-changemodelcontent', model)
+    })
+
+    return {
+      update(props) {
+        const { selection, value, start, left, zones } = props
+
+        // == diff selection
+        diffSelection(editor, prevProps.selection, selection)
+
+        // == diff value
+        if (model.getValue() !== value) {
+          model.setValue(value)
+        }
+
+        // == diff scroll positions
+        if (prevProps.start !== start) {
+          editor.setScrollTop(start * 18)
+        }
+        if (prevProps.left !== left) {
+          editor.setScrollLeft(left * 7.22)
+        }
+
+        // == diff zones
+        props.zones = diffZones(editor, prevProps.zones, zones)
+
+        // == end
+        prevProps = props
+      },
+      remove() {
+        onDidScrollChange.dispose()
+        onDidChangeCursorSelection.dispose()
+        editor.dispose()
+      },
+    }
+  },
+  { wrapperNode: <code data-monacoeditor /> },
+)
+
+function diffZones(
+  editor: mEditor.IStandaloneCodeEditor,
+  prevZones: Zone[] | null | undefined,
+  zones?: Zone[] | null | undefined,
+) {
+  let indexedByHeight = (prevZones || []).reduce((zones, zone) => {
+    let height = zone.heightInLines
+    zones[height] = zones[height] || []
+    zones[height].push(zone)
+    return zones
+  }, {} as { [height: string]: Zone[] })
+
+  let newZones: Zone[] = []
+
+  editor.changeViewZones(accessor => {
+    newZones = (zones || []).map(zone => {
+      let height = zone.heightInLines
+      const isNew = !indexedByHeight[height]?.length
+      if (isNew) {
+        zone.id = accessor.addZone({
+          domNode: document.createElement('div'),
+          ...zone,
+        })
+        return zone
+      }
+
+      let delegate = indexedByHeight[height].shift() as Zone
+      const areEquals =
+        delegate.afterLineNumber === zone.afterLineNumber &&
+        delegate.afterColumn === zone.afterColumn
+
+      if (!delegate.id || areEquals) {
+        return delegate
+      }
+
+      delegate.afterLineNumber = zone.afterLineNumber
+      delegate.afterColumn = zone.afterColumn
+      accessor.layoutZone(delegate.id)
+      return delegate
+    })
+
+    flatten(Object.values(indexedByHeight)).forEach(zone => {
+      zone.id && accessor.removeZone(zone.id)
+    })
+  })
+  return newZones
+}
+
+function diffSelection(
+  editor: mEditor.IStandaloneCodeEditor,
+  prevSelection: Selection | null | undefined,
+  selection?: Selection | null | undefined,
+): void {
+  if (!selection) {
+    if (prevSelection && !prevSelection.isEmpty()) {
+      prevSelection.collapseToStart()
+    }
+    return
+  }
+
+  if (!prevSelection || !prevSelection.equalsSelection(selection)) {
+    editor.setSelection(selection)
+    return
+  }
+}
+
+function emit<T extends Object = Object>(
+  elm: HTMLElement,
+  eventName: string,
+  detail: T,
+) {
+  elm.dispatchEvent(
+    new CustomEvent<T>(eventName, { detail, bubbles: true }),
+  )
+}
--- src/Editor/view/Buffer/index.tsx
+++ src/Editor/view/Buffer/index.tsx
@@ -1,16 +1,10 @@
 import './buffer.scss'
 import { VNodeStyle } from 'snabbdom/modules/style'
-import {
-  editor as mEditor,
-  Selection,
-  languages,
-  CancellationToken,
-} from 'monaco-editor'
-import { makeSnabbdomElement, Dict } from 'cycle-element/src'
-import { VNode, h } from '@cycle/dom'
-import { patch } from 'src/drivers'
+import { Selection } from 'monaco-editor'
+import { h } from '@cycle/dom'
 import merge from 'snabbdom-merge'
 import { BoundingRect } from 'src/lang'
+import { CodeEditor } from './CodeEditor'
 
 interface BufferElement {
   id?: string | number
@@ -28,121 +22,6 @@ interface BufferElement {
   voids: BoundingRect[]
   cuts: BoundingRect[]
 }
-
-type CodeEditorProps = {
-  selection?: Selection
-  value: string
-  start: number
-  left: number
-  children?: VNode[]
-  voids: BoundingRect[]
-  cuts: BoundingRect[]
-} & Dict
-
-let model: mEditor.ITextModel
-export const CodeEditor = makeSnabbdomElement<CodeEditorProps>(
-  elm => {
-    model = model || mEditor.createModel('', 'javascript')
-
-    let prevProps: Partial<CodeEditorProps> = {}
-    let editor = mEditor.create(elm, {
-      language: 'javascript',
-      roundedSelection: true,
-      scrollBeyondLastLine: false,
-      readOnly: false,
-      theme: 'vs-dark',
-      lineNumbers: 'on',
-      scrollbar: {
-        vertical: 'hidden',
-      },
-      automaticLayout: false,
-      minimap: {
-        enabled: false,
-      },
-      model,
-    })
-
-    let onDidScrollChange = editor.onDidScrollChange(() => {
-      const scrollTop = (prevProps.start || 0) * 18
-      const scrollLeft = (prevProps.left || 0) * 7.22
-      if (editor.getScrollTop() !== scrollTop) {
-        editor.setScrollTop(scrollTop)
-      }
-      if (editor.getScrollLeft() !== scrollLeft) {
-        editor.setScrollLeft(scrollLeft)
-      }
-    })
-
-    let onDidChangeCursorSelection = editor.onDidChangeCursorSelection(e => {
-      emit(elm, 'monaco-selectionchange', e)
-    })
-
-    editor.onDidChangeModelContent(e => {
-      emit(elm, 'monaco-changemodelcontent', model)
-    })
-
-    let vtree: VNode
-    let renderRoot = document.createElement('div')
-    elm.appendChild(renderRoot)
-
-    return {
-      update(props) {
-        const { selection, value, start, left, children } = props
-
-        // == diff selection
-        let editorSelection = editor.getSelection()
-        if (
-          selection &&
-          editorSelection &&
-          !selection.equalsSelection(editorSelection)
-        ) {
-          editor.setSelection(selection)
-        }
-
-        // == diff value
-        if (model.getValue() !== value) {
-          model.setValue(value)
-        }
-
-        // == diff scroll positions
-        if (prevProps.start !== start) {
-          editor.setScrollTop(start * 18)
-        }
-        if (prevProps.left !== left) {
-          editor.setScrollLeft(left * 7.22)
-        }
-
-        // // == diff children
-        // Promise.resolve().then(() => {
-        //   let newVtree = <div>{children}</div>
-        //   patch(vtree || renderRoot, newVtree)
-        //   vtree = newVtree
-        // })
-
-        // == view zones
-        editor.changeViewZones(accessor => {
-          props.voids.forEach(voidEl => {
-            let zoneId = accessor.addZone({
-              domNode: document.createElement('div'),
-              afterLineNumber: voidEl.lineStart + 1,
-              afterColumn: voidEl.columnStart,
-              heightInLines: voidEl.lineEnd - voidEl.lineStart + 1,
-            })
-          })
-        })
-
-        // == end
-        prevProps = props
-      },
-      remove() {
-        onDidScrollChange.dispose()
-        onDidChangeCursorSelection.dispose()
-        editor.dispose()
-      },
-    }
-  },
-  { wrapperNode: <code data-monacoeditor /> },
-)
 
 export function Buffer(props: BufferElement) {
   const {
@@ -170,7 +49,13 @@ export function Buffer(props: BufferElement) {
       value={value}
       left={left}
       start={start}
-      voids={voids}
+      zones={voids.map(zone => {
+        return {
+          afterLineNumber: zone.lineStart + 1,
+          afterColumn: zone.columnStart,
+          heightInLines: zone.lineEnd - zone.lineStart + 1,
+        }
+      })}
       cuts={cuts}
       data={{
         buffer: true,
@@ -192,15 +77,5 @@ export function Buffer(props: BufferElement) {
         .join(' ')}
     />,
     h('code', props),
-  )
-}
-
-function emit<T extends Object = Object>(
-  elm: HTMLElement,
-  eventName: string,
-  detail: T,
-) {
-  elm.dispatchEvent(
-    new CustomEvent<T>(eventName, { detail, bubbles: true }),
   )
 }
